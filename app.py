e2dc1f1f59a9106f435ffc3abc2cfa98989c55eb
Alice Yeh <aliceyeh12@gmail.com>
2020-11-06 10:15:45 -0800

    Initial test commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os 
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
